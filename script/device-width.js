122c36a881380ea7a933ffbb2e5e654384c0ed4f
Jugal Hero <jugalnepali@gmail.com>
2022-12-16 20:17:20 +0545

    Added New GUI

--- script/device-width.js
+++ script/device-width.js
@@ -1,13 +1,16 @@
 const navbar= document.getElementById('nav-bar-id')
 const navbarM= document.getElementById('nav-bar-id-m')
+const heroPageR= document.getElementById('hero-page-right-side-id')
 var width= window.innerWidth;
 if(width<820)
 {
     navbar.style.display= "none";
     navbarM.style.display="block";
+    heroPageR.style.display="none";
 }
 else{
     navbar.style.display="block";
+    heroPageR.style.display="flex";
     navbarM.style.display="none";
 }
 let scrollTop = window.scrollY;
